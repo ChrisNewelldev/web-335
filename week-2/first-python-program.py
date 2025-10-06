f9ef7ed6cf372ec582557155fbfa6703c9d59fb2
Chris Newell <84216346+ChrisNewelldev@users.noreply.github.com>
2025-10-06 08:32:31 -0400

    feat(week-2): add first Python program for Exercise 2.3

---
+++ week-2/first-python-program.py
@@ -0,0 +1,10 @@
+"""
+Author: Chris Newell
+Date: October 6, 2025
+File Name: first-python-program.py
+Description: This program prints a motivational quote and gives credit to its author.
+"""
+
+# Printing a motivational quote
+print("“The only limit to our realization of tomorrow is our doubts of today.”")
+print("- Franklin D. Roosevelt")

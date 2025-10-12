1315a0cbdfeb633164b2b0f143777ca7d164d55a
Chris Newell <84216346+ChrisNewelldev@users.noreply.github.com>
2025-10-12 14:01:12 -0500

    feat(week-4): add Assignment 4.3 MongoDB Shell queries

--- week-4/newell_assignment4.3.js
+++ week-4/newell_assignment4.3.js
@@ -1,21 +1,31 @@
-/*
-  Author: Chris Newell
-  Date: October 6, 2025
-  File Name: newell_assignment4.3.js
-  Description: MongoDB Shell queries for WEB-335 Assignment 4.3
-*/
+// Chris Newell - Assignment 4.3 - MongoDB Shell Queries
+// Course: WEB 335 - Node.js
+// Instructor: Richard Krasso
 
-// Display all users in the collection
+// These queries are intended to be run in mongosh after connecting to web335DB.
+
+use('web335DB');
+
+// Seed data (idempotent: clears and reinserts for demo)
+db.users.deleteMany({});
+db.users.insertMany([
+  { employeeId: 1007, firstName: 'Richard',  lastName: 'Bach',     email: 'rbach@me.com' },
+  { employeeId: 1008, firstName: 'Wolfgang', lastName: 'Mozart',   email: 'wmozart@me.com' },
+  { employeeId: 1009, firstName: 'Johann',   lastName: 'Bach',     email: 'jbach@me.com' },
+  { employeeId: 1010, firstName: 'Ludwig',   lastName: 'Beethoven',email: 'lbeethoven@me.com' }
+]);
+
+// a) Display all users in the collection
 db.users.find();
 
-// Display the user with the email address jbach@me.com
-db.users.find({ email: "jbach@me.com" });
+// b) Display the user with the email address jbach@me.com
+db.users.find({ email: 'jbach@me.com' });
 
-// Display the user with the last name Mozart
-db.users.find({ lastName: "Mozart" });
+// c) Display the user with the last name Mozart
+db.users.find({ lastName: 'Mozart' });
 
-// Display the user with the first name Richard
-db.users.find({ firstName: "Richard" });
+// d) Display the user with the first name Richard
+db.users.find({ firstName: 'Richard' });
 
-// Display the user with employeeId 1010
+// e) Display the user with employeeId 1010
 db.users.find({ employeeId: 1010 });
